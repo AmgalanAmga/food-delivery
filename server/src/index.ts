aef35667a00fef91b6fd2543a921564753a273b2
Amgalan <amka.storm9812@gmail.com>
2025-05-26 07:05:10 +0800

    asd

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import { configDotenv } from "dotenv";
 import { connectDatabase } from "./database";
@@ -10,6 +11,7 @@ connectDatabase();
 
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRouter);
